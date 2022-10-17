d82dd644da6f83aa6fc2f4dec99ce705f7e403b7
soundaryasrinivasagan <soundaryasrinivasagan@gmail.com>
2022-10-17 15:11:52 -0400

    table name input from user

--- main.py
+++ main.py
@@ -6,34 +6,46 @@
 
 import psycopg2
 import sys
-import pandas
 
-# Define our connection string
-conn_string = "host='localhost' dbname='increasecreditscore' user='soundaryasrinivasagan' password='secret'"
+def connectDBCreateTable(name_Table):
+	# Define our connection string
+	conn_string = "host='localhost' dbname='increasecreditscore' user='soundaryasrinivasagan' password='secret'"
 
-# Get a connection, if a connection cannot be made, an exception will be raised here
-conn = psycopg2.connect(conn_string)
+	# Get a connection, if a connection cannot be made, an exception will be raised here
+	conn = psycopg2.connect(conn_string)
 
-# conn.cursor will return a cursor object, you can use this cursor to perform queries
-conn.autocommit = True
-cursor = conn.cursor()
+	# conn.cursor will return a cursor object, you can use this cursor to perform queries
+	conn.autocommit = True
+	cursor = conn.cursor()
 
-name_Table = "news_stories12"
+	# Create table statement
+	sqlCreateTable = "create table " + name_Table + " (id bigint, title varchar(128), summary varchar(256), story text);"
 
-# Create table statement
-#sqlCreateTable = "create table " + name_Table + " (id bigint, title varchar(128), summary varchar(256), story text);"
+	# Create a table in PostgreSQL database
+	cursor.execute(sqlCreateTable)
+	conn.commit()
 
-# Create a table in PostgreSQL database
-#cursor.execute(sqlCreateTable)
-#conn.commit()
+	# This prints out all the columns in the table
+	sql= """ SELECT * FROM """ + name_Table + """ ; """
+	cursor.execute(sql)
+	column_names = [desc[0] for desc in cursor.description]
 
-sql= """ SELECT * FROM news_stories1; """
+	for i in column_names:
+		print(i)
 
-cursor.execute(sql)
-column_names = [desc[0] for desc in cursor.description]
+	# Close the connection
+	conn.commit()
+	conn.close()
 
-for i in column_names:
-    print(i)
+if __name__ == "__main__":
+	# Future version: check for invalid characters as input
+	val = input("Is this your first time running this program: [yes | no] ")
+	if ("yes" in val):
+		val_tableName = input("Please enter your Table Name: ")
+		# Run this statement once to initialize, need to find more permanent solution
+		connectDBCreateTable(val_tableName)
 
-conn.commit()
-conn.close()
+	elif("no" in val):
+		print("no")
+
+
