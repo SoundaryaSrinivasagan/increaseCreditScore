223acdde94d768e9649692e3481ace448d7c6687
SoundaryaSrinivasagan <soundaryasrinivasagan@gmail.com>
2022-10-16 20:18:35 -0400

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+
+# Author: Soundarya Srinivasagan
+# Date: October 16th, 2022
+# Project: increaseCreditScore
+# Version: Version_1
+# Description: This script will help the user strategize their income to effectively increase credit score
+
+
