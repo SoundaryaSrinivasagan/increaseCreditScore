1eb945f944e4e77831b994ecae390a542320b685
soundaryasrinivasagan <soundaryasrinivasagan@gmail.com>
2022-10-17 12:11:28 -0400

    db connection successful

--- main.py
+++ main.py
@@ -5,47 +5,22 @@
 # Version: Version_1
 # Description: This script will help the user strategize their income to effectively increase credit score
 
-'''
-# Creating the Database
-import psycopg2
-# pip3 install psycopg2-binary
-
-# Establishing the connection
-conn = psycopg2.connect(
-   database="postgres", user='soundarya', password='password', host='10.0.0.65', port= '5432'
-)
-conn.autocommit = True
-
-# Creating a cursor object using the cursor() method
-cursor = conn.cursor()
-
-# Preparing query to create a database
-#sql = CREATE database increaseCreditScore;
-
-# Creating a database
-cursor.execute(sql)
-print("Database created successfully...")
-
-# Closing the connection
-conn.close()
-'''
-#!/usr/bin/python
 import psycopg2
 import sys
 
 def main():
-	#Define our connection string
-	conn_string = "host='localhost' dbname='my_database' user='soundaryasrinivasagan' password='secret'"
+	# Define our connection string
+	conn_string = "host='localhost' dbname='increasecreditscore' user='soundaryasrinivasagan' password='secret'"
 
-	# print the connection string we will use to connect
-	print("Connecting to database\n	->%s" % (conn_string))
-
-	# get a connection, if a connect cannot be made an exception will be raised here
+	# Get a connection, if a connection cannot be made, an exception will be raised here
 	conn = psycopg2.connect(conn_string)
 
 	# conn.cursor will return a cursor object, you can use this cursor to perform queries
 	cursor = conn.cursor()
-	print("Connected!\n")
+	sql = ''' CREATE TABLE increasecreditscore.test(); '''
+	sql1 = ''' SELECT * from increasecreditscore.test; '''
+	cursor.execute(sql)
+	print(cursor.execute(sql1))
 
 if __name__ == "__main__":
 	main()
