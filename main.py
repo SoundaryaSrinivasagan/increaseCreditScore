a6cffabd9ad72a2ab0a2eea7c3e6ffc8c77b41ad
soundaryasrinivasagan <soundaryasrinivasagan@gmail.com>
2022-10-18 17:11:24 -0400

    fixed issue with update statement

--- main.py
+++ main.py
@@ -143,20 +143,23 @@ if __name__ == "__main__":
 	val_internet = input("What is the monthly amount that you pay for your internet: [Round up]  ")
 	val_phone = input("What is the monthly amount that you pay for your phone plan: [Round up]  ")
 	val_loans = input("What is the monthly amount that you pay for any loans (OSAP/PERSONAL): [Round up]  ")
+	print( "\n")
 	val_other = input ("Do you have any other monthly payments on your credit card: [yes | no] ")
 
 	# Dictionary to add all other types of payments
 	otherItemsList = {}
 
 	if ('yes' in val_other):
-		val_other = input("Please enter the name of the payment: ")
-		val_other_amount = input("What is the monthly amount that you pay for " + val_other + ": ")
+		val_other = input("Please enter the name of the payment:  ")
+		val_other_amount = input("What is the monthly amount that you pay for " + val_other + ": [Round up]  ")
+		print("\n")
 		otherItemsList.update({val_other : val_other_amount})
 		val_other1 = input("Do you have anymore payments: [yes | no]  ")
 
 		while ('yes' in val_other1):
 			val_other = input("Please enter the name of the payment: ")
-			val_other_amount = input("What is the monthly amount that you pay for " + val_other + ": ")
+			val_other_amount = input("What is the monthly amount that you pay for " + val_other + ": [Round up]  ")
+			print("\n")
 			otherItemsList.update({val_other : val_other_amount})
 			val_other1 = input("Do you have anymore payments: [yes | no]  ")
 
